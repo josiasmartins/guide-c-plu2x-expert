a635ba464cd8eb6861ffeb9000f6cb63d7b21492
Caitano <josiasmartinscaitano@gmail.com>
2023-05-19 08:36:46 -0300

    feat: adicionado module de sintaxe

---
+++ topics/1-sintaxe-basic/sintaxe.cpp
@@ -0,0 +1,43 @@
+// Declarações:
+int idade; // Declaração de uma variável inteira chamada "idade"
+float altura = 1.75; // Declaração e inicialização de uma variável float chamada "altura"
+void exibirMensagem(); // Declaração de uma função chamada "exibirMensagem"
+
+// Expressões:
+int x = 5;
+int y = 3;
+int resultado = x + y; // Expressão que adiciona as variáveis x e y
+float media = (x + y) / 2.0; // Expressão que calcula a média de x e y
+
+// Tipos de dados:
+int idade = 25; // Variável inteira
+float altura = 1.75; // Variável de ponto flutuante
+char letra = 'A'; // Variável de caractere
+bool status = true; // Variável booleana
+
+// Operadores:
+int x = 5;
+int y = 3;
+int soma = x + y; // Operador de adição
+int subtracao = x - y; // Operador de subtração
+int multiplicacao = x * y; // Operador de multiplicação
+float divisao = x / y; // Operador de divisão
+bool igualdade = (x == y); // Operador de igualdade
+
+
+// Fluxo de controle:
+int idade = 20;
+
+if (idade >= 18) {
+    cout << "Maior de idade" << endl;
+} else {
+    cout << "Menor de idade" << endl;
+}
+
+for (int i = 0; i < 5; i++) {
+    cout << i << " ";
+}
+
+while (idade < 30) {
+    idade++;
+}
